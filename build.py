90c23c2b14c8dcb4268eb0b8e5d7a461b80ea928
Eric J. Ma <ericmajinglong@gmail.com>
2016-11-27 15:33:32 -0500

    added thesis compilation script

---
+++ build.py
@@ -0,0 +1,22 @@
+"""
+Author: Eric J. Ma
+Purpose: A script to concatenate my thesis chapters together into a single
+         file. The key point here is that it adds a 'newline' character between
+         documents, something that the bash `cat` command doesn't do.
+"""
+
+order = ['title.md',
+         'introduction.md',
+         'algorithm.md',
+         'applications.md',
+         'future-work.md',
+         'appendices.md']
+
+thesis = ''
+for text in order:
+    with open(text, 'r+') as f:
+        thesis += f.read()
+        thesis += "\n"
+
+with open('thesis.md', 'w+') as f:
+    f.write(thesis)
