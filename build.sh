04437f6a4839a6e7bc5c16a4b262eb1ba807fab4
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-15 05:47:41 -0500

    updated build script with references for html

--- build.sh
+++ build.sh
@@ -2,16 +2,18 @@
 
 python concatenate.py
 
-pandoc thesis.md \
-    --template=default.latex \
-    -o thesis.pdf
-
-scp thesis.pdf doroot:/var/www/html/cv/.
+# pandoc thesis.md \
+#     --template=default.latex \
+#     -o thesis.pdf
+#
+# scp thesis.pdf doroot:/var/www/html/cv/.
 
 pandoc thesis.md \
     -o index.html \
     --template=default.html \
     -c styles.css \
+    --filter pandoc-citeproc \
+    --bibliography ../references-master/papers-library.bib
     -H header.html
 
 git add .
