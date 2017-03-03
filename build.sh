8b52f3e89df1488ed77411ae584399ddcaa97fde
Eric J. Ma <ericmajinglong@gmail.com>
2017-03-03 10:26:07 -0500

    moved PDF build to other script

--- build.sh
+++ build.sh
@@ -10,18 +10,6 @@ bash build_tables.sh
 # Build text
 bash build_text.sh
 
-pandoc thesis.md \
-    --template=default.latex \
-    --latex-engine=xelatex \
-    --filter pandoc-fignos \
-    --csl pnas.csl \
-    --filter pandoc-citeproc \
-    --bibliography papers-library.bib \
-    --toc \
-    -o thesis.pdf
-#
-# scp thesis.pdf doroot:/var/www/html/cv/.
-
 # Convert Markdown to HTML
 pandoc thesis.md \
     -o index.html \
